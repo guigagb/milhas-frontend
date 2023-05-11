373467e392675280e00fa7eb2050d9fb1dafd140
Guilherme <bsbtrindade@gmail.com>
2023-05-10 21:14:11 -0300

    up

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import Axios from 'axios'
+import Vue from 'vue';
+import Axios from 'axios';
 
-const defaultAxios = Axios
+const defaultAxios = Axios;
 
 const axios = Axios.create({
-  baseURL: process.env.VUE_APP_URL_SERVER || "http://192.168.100.200:1421/"
-})
+  baseURL: process.env.VUE_APP_URL_SERVER || "http://192.168.100.200:1421/",
+});
+
+axios.defaults.headers.common['Origin'] = 'https://milhas-frontend-r8utock3u-guigagb.vercel.app/';
 
 Vue.use({
   install(Vue) {
-    Vue.prototype.axios = axios
+    Vue.prototype.axios = axios;
   }
-})
+});
 
 export {
   defaultAxios,
   axios,
-}
+};
